161a5c7764752c7a18a60ab0c443e913d3a21940
Derwin <notderwin@gmail.com>
2023-06-07 23:22:02 +0700

    first commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+
+        <h1 className='text-3xl'>Hello</h1>
+
+
+
+
+    </div>
+  );  
+}
---
+++ src/pages/PlaygroundPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function PlaygroundPage() {
+  return (
+    <div>
+
+
+      <h1 className='underline'>Hi</h1>
+
+
+
+    </div>
+  );  
+}
